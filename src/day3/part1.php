2c7c8a3a9487012e2f3f0f965193204dda03adf5
Gergely Brautigam <skarlso777@gmail.com>
2016-05-13 08:46:11 +0200

    Day 3 part1.

--- src/day3/part1.php
+++ src/day3/part1.php
@@ -3,6 +3,34 @@ require "../helpers/timer.inc";
 
 $time_start = microtime(true);
 $content = file_get_contents('input.txt', true);
-foreach (str_split($content) as &$c) {
-    echo $c;
+$x = 0;
+$y = 0;
+$houses = [[]];
+$houses[$x][$y] = true;
+foreach (str_split($content) as $c) {
+    switch ($c) {
+        case '^':
+            $y++;
+            break;
+        case 'v':
+            $y--;
+            break;
+        case '<':
+            $x--;
+            break;
+        case '>':
+            $x++;
+            break;
+
+    }
+    $houses[$x][$y] = true;
 }
+
+$total = 0;
+foreach ($houses as $value) {
+    foreach ($value as $v) {
+        $total++;
+    }
+}
+printf("Total: %d\n", $total);
+DisplayElapsedTime($time_start);
