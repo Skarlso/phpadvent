2e980e470133f54a61981d6d94b6224d3a5368f3
Gergely Brautigam <skarlso777@gmail.com>
2016-05-17 20:00:54 +0200

    Day 6 part1.

--- src/day6/part1.php
+++ src/day6/part1.php
@@ -3,6 +3,70 @@ require "../helpers/timer.inc";
 
 $time_start = microtime(true);
 
-$grid = array_fill(0, 1000, array_fill(0, 1000, 0));
+function shutdown()
+{
+    // This is our shutdown function, in
+    // here we can do any last operations
+    // before the script is complete.
+    global $handle, $time_start;
+    if ($handle) {
+        print("Closing file handler.\n");
+        fclose($handle);
+    }
+    printf('Script executed with success:%s%s', PHP_EOL, "\n");
+    DisplayElapsedTime($time_start);
+}
+register_shutdown_function('shutdown');
 
-DisplayElapsedTime($time_start);
+$grid = array_fill(0, 1000, array_fill(0, 1000, 0));
+$handle = @fopen("input.txt", "r");
+
+if ($handle) {
+
+    while (($buffer = fgets($handle, 4096)) !== false) {
+        $fromX = 0;
+        $fromY = 0;
+        $toX = 0;
+        $toY = 0;
+        $value = 0;
+        $toggle = FALSE;
+        $split = explode(" ", $buffer);
+        if ($split[0] === "toggle") {
+            $numberOne = explode(",", $split[1]);
+            $numberTwo = explode(",", $split[3]);
+            $fromX = $numberOne[0];
+            $toX = $numberTwo[0];
+            $fromY = $numberOne[1];
+            $toY = $numberTwo[1];
+            $toggle = TRUE;
+        } elseif ($split[0] === "turn") {
+            $numberOne = explode(",", $split[2]);
+            $numberTwo = explode(",", $split[4]);
+            $fromX = $numberOne[0];
+            $toX = $numberTwo[0];
+            $fromY = $numberOne[1];
+            $toY = $numberTwo[1];
+            $value = $split[1] === "on" ? 1 : 0;
+        }
+
+        for ($x = $fromX; $x <= $toX; $x++) {
+            for ($y = $fromY; $y <= $toY; $y++) {
+                if ($toggle) {
+                    $grid[$x][$y] = 1 ^ $grid[$x][$y];
+                } else {
+                    $grid[$x][$y] = $value;
+                }
+            }
+        }
+    }
+}
+
+$total = 0;
+for ($x = 0; $x < 1000; $x++) {
+    for ($y = 0; $y < 1000; $y++) {
+        $total += $grid[$x][$y];
+    }
+}
+
+printf("Total is: %d\n", $total);
+
