c3ca9c531690617cb78ff9b71a6448b904217f2c
Gergely Brautigam <skarlso777@gmail.com>
2016-05-18 22:39:44 +0200

    Constructing the equation.

--- src/day7/part1.php
+++ src/day7/part1.php
@@ -3,8 +3,35 @@ require "../helpers/timer.inc";
 
 $time_start = microtime(true);
 
+/**
+ *
+ */
+class Equation
+{
+    private $hasValue, $value, $operator;
+
+    function __construct($hasValue, $value, $operator)
+    {
+        $this->$hasValue = $hasValue;
+        $this->$value = $value;
+        $this->$operator = $operator;
+    }
+
+    public function getHasValue()
+    {
+        return $this->$hasValue;
+    }
+
+    public function getValue()
+    {
+        return $this->$value;
+    }
+}
+
+
 $lines = file("input.txt");
 
-foreach ($lines as $line) {
+foreach ($lines as $line)
+{
     printf("%s", $line);
 }
