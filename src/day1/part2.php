74c70897468995ee96afc27812c3ba4096ce7646
Gergely Brautigam <skarlso777@gmail.com>
2016-05-23 12:33:17 +0200

    Closes #1.

--- src/day1/part2.php
+++ src/day1/part2.php
@@ -9,9 +9,8 @@ $floor = 0;
 $index = 0;
 $basementPosition = -1;
 
-$chars = str_split($movement);
-foreach($chars as $char){
-    $char == ')' ? $floor-- : $floor++;
+for ($i=0; $i < strlen($movement); $i++) {
+    $movement[$i] == ')' ? $floor-- : $floor++;
     if ($floor == -1 && $basementPosition == -1) {
         $basementPosition = $index;
     }
