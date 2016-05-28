c9a8e20920233e06e0f22a2df44cb110dcce8fda
Gergely Brautigam <skarlso777@gmail.com>
2016-05-28 20:19:56 +0200

    Part 1 solution day 10.

--- src/day10/part1.php
+++ src/day10/part1.php
@@ -2,3 +2,26 @@
 require "../helpers/timer.inc";
 
 $time_start = microtime(true);
+
+$input = "1321131112";
+
+for ($i=0; $i < 40; $i++) {
+    $count = 1;
+    $newInput = "";
+    for ($j=0, $_l = strlen($input); $j < $_l; $j++) {
+        if ($j+1 == $_l) {
+            $newInput .= $count . $input[$j];
+            break;
+        }
+
+        if ($input[$j] == $input[$j+1]) {
+            $count++;
+        } else {
+            $newInput .= $count . $input[$j];
+            $count = 1;
+        }
+    }
+    $input = $newInput;
+}
+
+printf("%s\n", strlen($input));
