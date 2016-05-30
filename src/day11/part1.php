a7465eb58ce2cdc34532e17cf422ca6aa2186094
Gergely Brautigam <skarlso777@gmail.com>
2016-05-30 07:33:09 +0200

    TBC

--- src/day11/part1.php
+++ src/day11/part1.php
@@ -6,6 +6,7 @@ $time_start = microtime(true);
 $start = "hxbxwxba";
 
 
+
 function overlapping($pass) {
     $pattern = '/(.)\1/';
     preg_match($pattern, $pass, $matches);
@@ -21,5 +22,17 @@ function matchCharactersNotPresent($pass) {
     if (count($matches) > 0) {
         return false;
     }
-    return true;    
+    return true;
+}
+
+function increment($pass, $n) {
+    $z = ord('z');
+    $a = ord('a');
+    //Use chr to go the other way.
+    if ($n == 0) {
+        $p = ord($pass[$n]);
+        $p++;
+        $p = $a % $z;
+        //TODO: To be continued.
+    }
 }
